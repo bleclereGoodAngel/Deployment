a8a18b2cc55a584e77cf9d9aebaafbf6eb47de64
Administrator <b.leclere@good-angel.fr>
2014-06-25 17:24:29 +0200

    Add missing component

--- Nuxeo/Deploy.sh
+++ Nuxeo/Deploy.sh
@@ -1,4 +1,5 @@
 ../Ubuntu.Update.sh
 wget -q -O- http://apt.nuxeo.org/nuxeo.key | sudo apt-key add -
+sudo apt-get install software-properties-common
 sudo add-apt-repository "deb http://apt.nuxeo.org/ precise fasttracks"
 sudo apt-get install nuxeo
