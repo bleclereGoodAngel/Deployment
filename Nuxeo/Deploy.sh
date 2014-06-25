6273acc402b52525862099e7a7f9600c6ef7e2e1
Administrator <b.leclere@good-angel.fr>
2014-06-25 18:07:02 +0200

    Add missing component ffmpeg

--- Nuxeo/Deploy.sh
+++ Nuxeo/Deploy.sh
@@ -2,4 +2,6 @@
 wget -q -O- http://apt.nuxeo.org/nuxeo.key | sudo apt-key add -
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository "deb http://apt.nuxeo.org/ precise fasttracks"
-sudo apt-get install nuxeo
+sudo add-apt-repository ppa:jon-severinsson/ffmpeg && sudo apt-get update -qq
+sudo apt-get update
+sudo apt-get install nuxeo -y
