f4ce1d4b049d02f1ced5c0be6222f4ed84d78b90
Administrator <b.leclere@good-angel.fr>
2014-06-25 16:47:27 +0200

    Add webmin deploy script

---
+++ WebMin/Deploy.sh
@@ -0,0 +1,6 @@
+sudo bash ../Ubuntu.Update.sh
+wget -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add -
+echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
+echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" >> /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install webmin
