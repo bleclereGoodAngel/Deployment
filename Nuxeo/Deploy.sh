1a4b91f3039c60b0ee05bdd3882a7f9ef4ec70bb
Administrator <b.leclere@good-angel.fr>
2014-06-25 17:25:29 +0200

    Force install

--- Nuxeo/Deploy.sh
+++ Nuxeo/Deploy.sh
@@ -1,5 +1,5 @@
 ../Ubuntu.Update.sh
 wget -q -O- http://apt.nuxeo.org/nuxeo.key | sudo apt-key add -
-sudo apt-get install software-properties-common
+sudo apt-get install software-properties-common -y
 sudo add-apt-repository "deb http://apt.nuxeo.org/ precise fasttracks"
 sudo apt-get install nuxeo
