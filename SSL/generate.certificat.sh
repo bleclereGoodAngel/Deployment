818521e1f5f76464ebcce5bd4ceb463f21c7a1f4
Administrator <b.leclere@good-angel.fr>
2014-06-26 09:18:33 +0200

    Missing number of day

--- SSL/generate.certificat.sh
+++ SSL/generate.certificat.sh
@@ -1,7 +1,14 @@
 certificatName=Metatron
 
+sudo mkdir tmp.$certificatName
+
+pushd tmp.$certificatName
+
 sudo openssl req -new > $certificatName.csr
 sudo openssl rsa -in privkey.pem -out new.cert.key
-sudo openssl x509 -in $certificatName.csr -out new.cert.cert -req -signkey new.cert.key -days NNN
+sudo openssl x509 -in $certificatName.csr -out new.cert.cert -req -signkey new.cert.key -days 365
 sudo cp new.cert.cert /etc/ssl/certs/$certificatName.crt
 sudo cp new.cert.key /etc/ssl/private/$certificatName.key
+
+popd
+sudo rm -rf tmp.$certificatName
