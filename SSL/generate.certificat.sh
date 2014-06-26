8dc2abcfd416d57d418592b436d14be14ef8c721
Administrator <b.leclere@good-angel.fr>
2014-06-26 09:10:18 +0200

    Add ssl certificat generation script

---
+++ SSL/generate.certificat.sh
@@ -0,0 +1,7 @@
+certificatName=Metatron
+
+sudo openssl req -new > $certificatName.csr
+sudo openssl rsa -in privkey.pem -out new.cert.key
+sudo openssl x509 -in new.cert.csr -out new.cert.cert -req -signkey new.cert.key -days NNN
+sudo cp new.cert.cert /etc/ssl/certs/$certificatName.crt
+sudo cp new.cert.key /etc/ssl/private/$certificatName.key
