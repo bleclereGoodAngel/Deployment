a2380b14b6d597fb33964a020a7838535f91caad
Administrator <b.leclere@good-angel.fr>
2014-06-26 09:13:01 +0200

    Missing argument in ssl certificat generation

--- SSL/generate.certificat.sh
+++ SSL/generate.certificat.sh
@@ -2,6 +2,6 @@ certificatName=Metatron
 
 sudo openssl req -new > $certificatName.csr
 sudo openssl rsa -in privkey.pem -out new.cert.key
-sudo openssl x509 -in new.cert.csr -out new.cert.cert -req -signkey new.cert.key -days NNN
+sudo openssl x509 -in $certificatName.csr -out new.cert.cert -req -signkey new.cert.key -days NNN
 sudo cp new.cert.cert /etc/ssl/certs/$certificatName.crt
 sudo cp new.cert.key /etc/ssl/private/$certificatName.key
