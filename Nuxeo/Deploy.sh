62bb67369bfa9e440951c19113a3537ae9d1a245
Administrator <b.leclere@good-angel.fr>
2014-06-27 14:39:02 +0200

    mysql server missing in own cloud

--- Nuxeo/Deploy.sh
+++ Nuxeo/Deploy.sh
@@ -4,4 +4,5 @@ sudo apt-get install software-properties-common -y
 sudo add-apt-repository "deb http://apt.nuxeo.org/ precise fasttracks"
 sudo add-apt-repository ppa:jon-severinsson/ffmpeg && sudo apt-get update -qq
 sudo apt-get update
+sudo apt-get install ffmpeg
 sudo apt-get install nuxeo -y
--- owncloud/deploy.sh
+++ owncloud/deploy.sh
@@ -1,4 +1,6 @@
 sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install mysql-server -y
 sudo apt-get install owncloud -y
 sudo a2enmod ssl
 sudo a2ensite default-ssl
