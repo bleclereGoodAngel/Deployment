a38781eee42260a8725682b677cc42d3dd7ee6d1
Administrator <b.leclere@good-angel.fr>
2014-07-25 17:28:26 +0200

    Update gitlab

--- Gitlab/Deploy.sh
+++ Gitlab/Deploy.sh
@@ -2,8 +2,10 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo mkdir gitlab.install
 pushd gitlab.install
-wget https://downloads-packages.s3.amazonaws.com/ubuntu-12.04/gitlab_6.9.2-omnibus.1-1_amd64.deb
-sudo dpkg -i gitlab_6.9.2-omnibus.1-1_amd64.deb
+wget https://downloads-packages.s3.amazonaws.com/ubuntu-14.04/gitlab_7.1.0-omnibus-1_amd64.deb
+sudo apt-get install openssh-server
+sudo apt-get install postfix # Select 'Internet Site', using sendmail or exim is also OK
+sudo dpkg -i gitlab_7.1.0-omnibus-1_amd64.deb
 popd
 rm -rf gitlab.install
 sudo gitlab-ctl reconfigure
